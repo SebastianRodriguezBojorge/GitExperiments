09b0f18f26d0d8dddc1e6516a71c375b86d1a8eb
Sebastian Rodriguez Bojorge <SebastianRodriguezBojorge@users.noreply.github.com>
2020-03-30 23:28:45 -0700

    Adding sln with two basic projects

---
+++ GitExp/GitExp/GitExp_Calc/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitExp_Calc
+{
+    public static class Calc
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ GitExp/GitExp/GitExp_CalcToolkit/CalcToolkit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitExp_CalcToolkit
+{
+    public static class CalcToolkit
+    {
+        public static int Add(int a, int b)
+        {
+            return GitExp_Calc.Calc.Add(a, b);
+        }
+    }
+}
